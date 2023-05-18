52392c73ffe3018c7a84f3ba9b079ee457de61da
ahmed <axmedarna3@gmail.com>
2023-05-18 14:29:58 +0300

    initial commit

---
+++ linkedlists.js
@@ -0,0 +1,35 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+      this.length++;
+      return this;
+    }
+  }
+  unshift() {}
+  insert() {}
+}
+
+const newLinkedList = new LinkedList(10);
+newLinkedList.push(20);
+console.log(newLinkedList.tail);
